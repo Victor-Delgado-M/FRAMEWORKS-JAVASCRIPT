6066afa949929940859e5fe554fb05b9c1fb3638
Victor-Delgado-M <94375075+Victor-Delgado-M@users.noreply.github.com>
2022-12-04 19:14:44 -0500

    Add files via upload

---
+++ FRAMEWORKS JAVASCRIPT/src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+  <PrincipalHeader />
+
+
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+// Components
+
+
+import PrincipalHeader from '../components/PrincipalHeader.vue';
+
+
+export default defineComponent({
+  name: 'HomeView',
+
+  components: {
+
+    PrincipalHeader,
+   
+  },
+});
+</script>
